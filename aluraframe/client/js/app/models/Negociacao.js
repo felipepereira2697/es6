52464c34af45e8b12e8ba0680d90b8c7fcd61811
Felipe Pereira - PERSONAL <felipepereira2697@gmail.com>
2020-01-26 14:00:08 -0300

    Usando a api de Date com programacao defensiva

--- aluraframe/client/js/app/models/Negociacao.js
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -5,18 +5,24 @@ class Negociacao {
         //que uma propriedade só pode ser mudada a partir da propria classe (metodos) nos usamos o _ na frente do nome da propriedade
         //Perceba que isso é uma CONVENCAO, o programador ao ver isso deve entender q ele NAO DEVE ACESSAR A PROPRIEDADE DIRETAMENTE
 
-        this._data = data;
+        this._data = new Date(data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;
         //Quando eu uso o freeze estou congelando o objeto e assim nao é possivel alterar nenhuma propriedade desse cara
         //nesse caso funciona pois nem os metodos da nossa classe alteram as propriedades da mesma.
+        //Object freeze é shallow oq significa q ele congela somente as propriedades desse objeto, no caso de objetos dentro de objetos
+        //esses objetos internos, nao tem suas propriedades congeladas, e isso portanto nao garante a imutabilidade total
         Object.freeze(this);
     }
     get volume() {
         return this._quantidade * this._valor;
     }
     get data() {
-        return this._data;
+        //Fazendo o uso de programacao defensiva, exemplo do date, se nao retornamos o usando uma instancia nova
+        //o programador pode usar n1.data.setDate(dia) para alterar a data e nao queremos isso
+        //pra isso usamos o new Date e pegamos o time a partir da data q foi originalmente definida, assim, quando 
+        //um programador tentar alterar a data, ele nao vai pegar a data formatada e sim um Long gigantesco
+        return new Date(this._data.getTime());
     }
 
     get quantidade() {
