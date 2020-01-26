f0a01254e2fad4fa6131033443413abf09f665e3
Felipe Pereira - PERSONAL <felipepereira2697@gmail.com>
2020-01-26 12:41:12 -0300

    Metodo de obtemVolume()

--- aluraframe/client/js/app/models/Negociacao.js
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -1,9 +1,11 @@
 class Negociacao {
-    constructor() {
+    constructor(data, quantidade, valor) {
         //o this sempre aponta pra instancia que ta executando aquele momento
-        this.data = new Date();
-        this.quantidade = 1;
-        this.valor = 0;
-        
+        this.data = data;
+        this.quantidade = quantidade;
+        this.valor = valor;
+    }
+    obtemVolume() {
+        return this.quantidade * this.valor;
     }
 }
