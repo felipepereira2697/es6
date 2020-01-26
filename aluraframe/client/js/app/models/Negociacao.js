9b36119acfeea775bc9c34d463992923967178a2
Felipe Pereira - PERSONAL <felipepereira2697@gmail.com>
2020-01-26 12:31:40 -0300

    Criando a classe Negociacao.js

---
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -0,0 +1,9 @@
+class Negociacao {
+    constructor() {
+        //o this sempre aponta pra instancia que ta executando aquele momento
+        this.data = new Date();
+        this.quantidade = 1;
+        this.valor = 0;
+        
+    }
+}
