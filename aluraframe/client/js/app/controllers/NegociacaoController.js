a53e6413e6eb0941335d87fc3de9fa8e35b78acb
Felipe Pereira - PERSONAL <felipepereira2697@gmail.com>
2020-01-27 23:27:58 -0300

    Criando o modelo de listade negociacoes

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -15,19 +15,30 @@ class NegociacaoController {
        this._inputDate = $("#data"); 
        this._inputQuantidade = $("#quantidade");
        this._inputValor = $("#valor");
+       this._listaNegociacoes = new ListaNegociacoes();
     }
     adiciona(event) {
         event.preventDefault();
        
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
+        console.log(this._listaNegociacoes.negociacoes);
         
-        let data = DateHelper.textoParaData(this._inputDate.value);
-        let negociacao = new Negociacao(
-            data,
-            this._inputQuantidade.value,
-            this._inputValor.value
-        )
-        console.log(negociacao);
-        console.log(DateHelper.dataParaTexto(negociacao.data));
-        
+    }
+    /*Só a classe pode chamar esses metodos com _ */
+    _criaNegociacao() {
+         
+        return  new Negociacao(
+                    DateHelper.textoParaData(this._inputDate.value),
+                    this._inputQuantidade.value,
+                    this._inputValor.value
+                    )
+    }
+    _limpaFormulario() {
+        this._inputDate.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor = 0.0;
+
+        this._inputDate.focus();
     }
 }
