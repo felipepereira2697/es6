a09e5b0213689349fd1ea130c554dad8bd3f2af6
Felipe Pereira - PERSONAL <felipepereira2697@gmail.com>
2020-01-27 22:53:37 -0300

    usando template string e tambem usando expressoes regulares para validar o formato da data

--- aluraframe/client/js/app/helpers/DateHelper.js
+++ aluraframe/client/js/app/helpers/DateHelper.js
@@ -6,6 +6,9 @@ class DateHelper {
     //Como aqui nao temos propriedade nao instanciamos nada, nao usamos constructor customizado, esses metodos de helper
     //fazem sentido ser static
     static textoParaData(texto) {
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto)) {
+            throw new Error('Deve estar no formato ano mes dia aaaa/mm/dd');
+        }
          //Podemos usar o spread operator, assim quando passamos um array pra dentro de um metodo ele entende
         //que a primeira posicao do array é o primeiro parametro da funcao e assim por diante
 
@@ -13,10 +16,13 @@ class DateHelper {
             Podemos usar arrow function no caso do map e quando temos somente UMA instrucao
             podemos emitir as chaves do bloco bem como a palavra return
         */
+
         return new Date(...texto.split('-')
                     .map((item, indice) => indice == 1 ? item - 1: item));
     }
     static dataParaTexto(data) {
-        return data.getDate() + '/'+(data.getMonth()+1) + '/'+data.getFullYear();
+
+        return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
+        
     }
 }
