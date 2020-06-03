4bb25e837d7450907833361ba2f180460cb18968
Felipe Pereira - PERSONAL <felipepereira2697@gmail.com>
2020-06-02 23:01:57 -0300

    Adding and learning about request param, query param and request body

--- nlw/server/src/server.ts
+++ nlw/server/src/server.ts
@@ -1,18 +1,45 @@
 import express from 'express';
 
 
-
+const users = ['Cruyff', 'Pelé', 'Michael Jordan', 'Tom Brady', 'Ronaldo', 'Cristiano Ronaldo'];
 const app = express();
+
+//vamos adicionar um plugin para que o express entenda que vamos conversar em json com ele
+app.use(express.json());
 //Rota: Endereco completo da requisicao
 //Recurso: Entidade que estamos acessando do sistema
-app.get('/users',(request,response) => {
-    return  response.json(['Cruyff', 'Pelé', 'Michael Jordan', 'Tom Brady', 'Ronaldo', 'Cristiano Ronaldo']);
-})
 
+
+//A respeito de parametros
+//Request Param: Parametros que vem na propria rota e identificam um recurso
+//Query param: tbm vem na propria rota mas diferente do request param, o query param pode ser opcional muito usado para filtros, paginacao
+//Request body:parametros para criacao/atualizacao de informacoes
+app.get('/users',(request,response) => {
+    //Quem determina o query param é quem ta chamando
+    //como pode vir undefined por ser opcional, precisamos checar
+    //O query param pode trazer mais de um valor pra search
+    //mas como nao estamos preocupados com isso agora, vamos forcar via cast pra ele ser uma string
+    const search = String(request.query.search);
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+    return  response.json(filteredUsers);
+})
+//PAssando parametros
+app.get('/users/:id', (request, response) => {
+    //pegando o parametro pelo nome dele
+    const id = Number(request.params.id);
+    const user = users[id];
+
+    return response.json(user);
+
+
+})
 app.post('/users',(request, response) => {
+
+    const data = request.body;
+    console.log(data);
     const user = {
-        name: 'Pelé',
-        email: 'thegoat@gmail.com'
+        name: data.name,
+        email: data.email
     }
     return response.json(user);
 })
