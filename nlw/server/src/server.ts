0d9dd010862fcaa236fd8da6151d84b1753d3142
Felipe Pereira - PERSONAL <felipepereira2697@gmail.com>
2020-06-02 22:01:14 -0300

    Creating post method

--- nlw/server/src/server.ts
+++ nlw/server/src/server.ts
@@ -1,11 +1,21 @@
 import express from 'express';
 
 
+
 const app = express();
+//Rota: Endereco completo da requisicao
+//Recurso: Entidade que estamos acessando do sistema
 app.get('/users',(request,response) => {
-    response.json(['Cruyff', 'Pelé', 'Michael Jordan', 'Tom Brady', 'Ronaldo', 'Cristiano Ronaldo']);
+    return  response.json(['Cruyff', 'Pelé', 'Michael Jordan', 'Tom Brady', 'Ronaldo', 'Cristiano Ronaldo']);
 })
 
+app.post('/users',(request, response) => {
+    const user = {
+        name: 'Pelé',
+        email: 'thegoat@gmail.com'
+    }
+    return response.json(user);
+})
 app.listen(3333);
 
 
