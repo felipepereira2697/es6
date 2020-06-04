106aa1e10515fb0493b204ad961d089f11b12011
Felipe Pereira - PERSONAL <felipepereira2697@gmail.com>
2020-06-03 22:54:33 -0300

    Criando listagem de itens

--- nlw/server/src/routes.ts
+++ nlw/server/src/routes.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import knex from './database/connection';
 
 //Rota: Endereco completo da requisicao
 //Recurso: Entidade que estamos acessando do sistema
@@ -9,10 +10,20 @@ const routes = express.Router();
 //Request Param: Parametros que vem na propria rota e identificam um recurso
 //Query param: tbm vem na propria rota mas diferente do request param, o query param pode ser opcional muito usado para filtros, paginacao
 //Request body:parametros para criacao/atualizacao de informacoes
-routes.get('/',(request,response) => {
-    return response.json({
-        message : 'Hey Felipe Pereira'
+routes.get('/items',async (request,response) => {
+    //como queries demoram no bd, precisamos usar o await
+    const items = await knex('items').select('*');
+
+    //nem sempre as infos do bd vem da forma que vc quer mostrar no front, entao vc pode serializar
+    // transformar dados em um novo formato pra ficar mais facil
+    const serializedItems = items.map(item => {
+        return {
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`,
+        }
     })
+
+    return response.json(serializedItems);
 })
 
 export default routes;
--- nlw/server/src/server.ts
+++ nlw/server/src/server.ts
@@ -1,5 +1,6 @@
 //só nao tem ./ os arquivos que vem do node modules
 import express from 'express';
+import path from 'path';
 import routes from './routes';
 
 const app = express();
@@ -7,6 +8,9 @@ const app = express();
 //vamos adicionar um plugin para que o express entenda que vamos conversar em json com ele
 app.use(express.json());
 app.use(routes);
+
+//recursos estatico
+app.use('/uploads', express.static(path.resolve(__dirname,'..','uploads')));
 
 
 app.listen(3333);
