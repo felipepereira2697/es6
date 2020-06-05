b587c3337f3c7aafbbcf94000f85340ed2878335
Felipe Pereira - PERSONAL <felipepereira2697@gmail.com>
2020-06-04 21:09:53 -0300

    Creating, items, points and point_items

--- nlw/server/src/routes.ts
+++ nlw/server/src/routes.ts
@@ -18,12 +18,43 @@ routes.get('/items',async (request,response) => {
     // transformar dados em um novo formato pra ficar mais facil
     const serializedItems = items.map(item => {
         return {
-            title: item.title,
-            image_url: `http://localhost:3333/uploads/${item.image}`,
+            id          : item.id,
+            title       : item.title,
+            image_url   : `http://localhost:3333/uploads/${item.image}`,
         }
     })
 
     return response.json(serializedItems);
 })
+
+routes.post('/points', async (request, response) => {
+    const {
+        name, email,whatsapp, latitude, longitude,city, uf, items
+    } = request.body;
+   
+    const insertedIds = await knex('points').insert({
+        image : 'image-fake',
+        name : name, 
+        email : email,
+        whatsapp : whatsapp, 
+        latitude : latitude, 
+        longitude : longitude,
+        city : city,
+        uf : uf,
+    });
+    
+    const point_id = insertedIds[0];
+    const pointItems = items.map((item_id : number) => {
+        return {
+            item_id,
+            point_id : point_id,
+        }
+    })
+
+    await knex('point_items').insert(pointItems);
+    return response.json({success : true});
+})
+
+
 
 export default routes;
