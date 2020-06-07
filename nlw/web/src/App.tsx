5a9aa6fdbd2a40321f92effb67c8f58a35ad9142
Felipe Pereira - PERSONAL <felipepereira2697@gmail.com>
2020-06-07 17:15:27 -0300

    Fist component, header

--- nlw/web/src/App.tsx
+++ nlw/web/src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 import Header from './Header';
+import { useState } from 'react';
 
 function App() {
   return (
     <div>
-      <Header />
+      <Header title="Batatinha" />
       <h1>Hello batatinha</h1>
     </div>
     
--- nlw/web/src/Header.tsx
+++ nlw/web/src/Header.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
+interface HeaderProps {
+    title: string;
+}
 //podemos guardar como constantes
-const Header = () => {
+//O TIPO DE HEADER SERÁ REACT.FC QUE SIGNIFICA Function component
+//um componente escrito em formato de funcao
+//esse FC é um generic do Typescript q pode receber um parametro, informando quais propriedades 
+//esse cara pode receber
+
+const Header: React.FC<HeaderProps> = (props) => {
     return(
         <header>
-            <h1>Ecoleta</h1>
+            <h1>{props.title}</h1>
         </header>
     )
 }
