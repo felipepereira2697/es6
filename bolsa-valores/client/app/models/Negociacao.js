3fe73d8d8f49d7c079ab5080b13bfd0f0bc66243
Felipe Pereira - PERSONAL <felipepereira2697@gmail.com>
2020-06-09 21:05:25 -0300

    Criando a classe Negociacao

---
+++ bolsa-valores/client/app/models/Negociacao.js
@@ -0,0 +1,9 @@
+class Negociacao {
+
+    //atributos sao definidos atraves do construtor
+    constructor(){
+        this.data = new Date();
+        this.quantidade = 1;
+        this.valor = 0.0;
+    }
+}
