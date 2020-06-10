b7388d6128429575d3f60c14446c2d162c240df8
Felipe Pereira - PERSONAL <felipepereira2697@gmail.com>
2020-06-09 21:11:04 -0300

    Adicionando comportamentos a classe Negocicao

--- bolsa-valores/client/app/models/Negociacao.js
+++ bolsa-valores/client/app/models/Negociacao.js
@@ -1,9 +1,13 @@
 class Negociacao {
 
     //atributos sao definidos atraves do construtor
-    constructor(){
-        this.data = new Date();
-        this.quantidade = 1;
-        this.valor = 0.0;
+    constructor(data, quantidade, valor){
+        this.data = data
+        this.quantidade = quantidade;
+        this.valor = valor;
+    }
+
+    volume() {
+        return this.quantidade * this.valor;
     }
 }
