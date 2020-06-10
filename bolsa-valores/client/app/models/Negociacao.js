8151aeed5db8e1828fcc74c358cd86ba20da0f2f
Felipe Pereira - PERSONAL <felipepereira2697@gmail.com>
2020-06-09 21:20:23 -0300

    Adicionando modificadores de acesso

--- bolsa-valores/client/app/models/Negociacao.js
+++ bolsa-valores/client/app/models/Negociacao.js
@@ -2,12 +2,22 @@ class Negociacao {
 
     //atributos sao definidos atraves do construtor
     constructor(data, quantidade, valor){
-        this.data = data
-        this.quantidade = quantidade;
-        this.valor = valor;
+        this._data = data
+        this._quantidade = quantidade;
+        this._valor = valor;
     }
 
-    volume() {
-        return this.quantidade * this.valor;
+    getVolume() {
+        return this.getQuantidade() * this.getValor();
+    }
+    //acessadores
+    getData() {
+        return this._data;
+    }
+    getQuantidade() {
+        return this._quantidade;
+    }
+    getValor() {
+        return this._valor;
     }
 }
