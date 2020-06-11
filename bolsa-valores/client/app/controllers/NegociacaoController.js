c60a18b85017f653f566530f810869217fbcbb96
Felipe Pereira - PERSONAL <felipepereira2697@gmail.com>
2020-06-10 23:11:34 -0300

    Adicionando propriedades para NegociacaoController

--- bolsa-valores/client/app/controllers/NegociacaoController.js
+++ bolsa-valores/client/app/controllers/NegociacaoController.js
@@ -1,8 +1,18 @@
 class NegociacaoController{
 
+    //para evitar percorrer o dom muitas vezes vamos colocar quantidade, valor e data como atributos da controller
+    constructor() {
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputData       = document.querySelector('#data');
+        this.inputValor      = document.querySelector('#valor');
+
+    }
     adiciona(event) {
         //se vc nao cancelar o comportamento padrao do form ele vai recarregar, e ai perder infos
         event.preventDefault();
         
+        console.log(this.inputData.value);
+        console.log(this.inputValor.value);
+        console.log(this.inputQuantidade.value);
     }
 }
