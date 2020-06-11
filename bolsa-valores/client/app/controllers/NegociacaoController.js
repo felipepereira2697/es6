11f12d1a453c2faa022dbe9d8018997a157505ac
Felipe Pereira - PERSONAL <felipepereira2697@gmail.com>
2020-06-10 23:35:32 -0300

    Aprendendo a lidar com o objeto Date do js

--- bolsa-valores/client/app/controllers/NegociacaoController.js
+++ bolsa-valores/client/app/controllers/NegociacaoController.js
@@ -2,17 +2,17 @@ class NegociacaoController{
 
     //para evitar percorrer o dom muitas vezes vamos colocar quantidade, valor e data como atributos da controller
     constructor() {
-        this.inputQuantidade = document.querySelector('#quantidade');
-        this.inputData       = document.querySelector('#data');
-        this.inputValor      = document.querySelector('#valor');
+        this._inputQuantidade = document.querySelector('#quantidade');
+        this._inputData       = document.querySelector('#data');
+        this._inputValor      = document.querySelector('#valor');
 
     }
     adiciona(event) {
         //se vc nao cancelar o comportamento padrao do form ele vai recarregar, e ai perder infos
         event.preventDefault();
-        
-        console.log(this.inputData.value);
-        console.log(this.inputValor.value);
-        console.log(this.inputQuantidade.value);
+        console.log(this._inputData);
+        //recebe um array ano, mes e dia
+        let data = new Date(this._inputData.value.split('-'));
+        console.log(data);
     }
 }
