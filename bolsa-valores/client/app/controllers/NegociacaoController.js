dbec3e8837ee8a46c5da29ac151457a83f1d2849
Felipe Pereira - PERSONAL <felipepereira2697@gmail.com>
2020-06-10 22:54:27 -0300

    Creating new controller

---
+++ bolsa-valores/client/app/controllers/NegociacaoController.js
@@ -0,0 +1,8 @@
+class NegociacaoController{
+
+    adiciona(event) {
+        //se vc nao cancelar o comportamento padrao do form ele vai recarregar, e ai perder infos
+        event.preventDefault();
+        
+    }
+}
