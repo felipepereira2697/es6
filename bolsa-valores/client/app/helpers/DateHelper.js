c2db956dd0e8982554dc0a32702df607b1e0b182
Felipe Pereira - PERSONAL <felipepereira2697@gmail.com>
2020-06-11 18:29:13 -0300

    Expressao regular, baseando-se em fail fast

--- bolsa-valores/client/app/helpers/DateHelper.js
+++ bolsa-valores/client/app/helpers/DateHelper.js
@@ -5,6 +5,9 @@ class DateHelper {
     }
 
     static textoParaData(texto) {
+        if(!(/\d{4}-\d{2}-\d{2}/.test(texto))) {
+            throw new Error('Data deve estar no formato yyyy-mm-dd ');
+        }
          //recebe um array ano, mes e dia
         //quando usa o spread significa q ele tem que ser desmembrado
         return new Date(...texto
@@ -20,6 +23,8 @@ class DateHelper {
     }
 
     static dataParaTexto(data) {
-        return data.getDate() + '/'+(data.getMonth()+1)+'/'+data.getFullYear();
+        let mes = (data.getMonth()+1);
+
+        return `${data.getDate()}/${mes}/${data.getFullYear()}`;
     }
 }
