db30ad2f46706fdb8efa7b228815e65135146a5b
Felipe Pereira - PERSONAL <felipepereira2697@gmail.com>
2020-06-11 20:13:17 -0300

    Refatorando e adicionando uma negociacao ao modelo de lista de negociacoes

--- bolsa-valores/client/app/controllers/NegociacaoController.js
+++ bolsa-valores/client/app/controllers/NegociacaoController.js
@@ -5,16 +5,30 @@ class NegociacaoController{
         this._inputQuantidade = document.querySelector('#quantidade');
         this._inputData       = document.querySelector('#data');
         this._inputValor      = document.querySelector('#valor');
+        this._listaNegociacoes  = new ListaNegociacoes();
 
     }
     adiciona(event) {
         //se vc nao cancelar o comportamento padrao do form ele vai recarregar, e ai perder infos
         event.preventDefault();
 
-        let negociacao = new Negociacao(DateHelper.textoParaData(this._inputData.value), this._inputQuantidade.value, this._inputValor.value);
-        console.log('Negociacao: ',negociacao);
-
-        console.log('Negociacao data formatada ',DateHelper.dataParaTexto(negociacao.data));
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
+        console.log(this._listaNegociacoes.negociacoes);
     }
+
+    //metodos privado
+    _criaNegociacao() {
+        return new Negociacao(DateHelper.textoParaData(this._inputData.value), this._inputQuantidade.value, this._inputValor.value);
+    }
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+
+        this._inputData.focus();
+    }
+
+
 
 }
