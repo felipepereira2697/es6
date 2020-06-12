fc818a81a882fdd7fef7053c62c90942da764fd9
Felipe Pereira - PERSONAL <felipepereira2697@gmail.com>
2020-06-11 22:00:13 -0300

    Programacao defensiva para evitar acesso a lista de negociacoes de forma indevida

--- bolsa-valores/client/app/models/ListaNegociacoes.js
+++ bolsa-valores/client/app/models/ListaNegociacoes.js
@@ -9,7 +9,11 @@ class ListaNegociacoes {
     }
 
     get negociacoes(){
-        return this._negociacoes;
+        //programacao defensiva para evitar problemas como adicionar ou limpar a lista sem usar os metodos corretos
+        //para isso vamos retornar uma instancia nova de lista de negociacoes
+        //vamos usar o metodo concat para criar um novo array com o conteudo de this._negociacoes
+        return [].concat(this._negociacoes);
+        
     }
 
 
