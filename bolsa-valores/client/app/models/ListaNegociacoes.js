5c8ce39b6d51278017e778ee2e72a361add7af8c
Felipe Pereira - PERSONAL <felipepereira2697@gmail.com>
2020-06-16 22:27:05 -0300

    adicionando comentario

--- bolsa-valores/client/app/models/ListaNegociacoes.js
+++ bolsa-valores/client/app/models/ListaNegociacoes.js
@@ -15,7 +15,7 @@ class ListaNegociacoes {
     }
 
     get negociacoes(){
-        //programacao defensiva para evitar problemas como adicionar ou limpar a lista sem usar os metodos corretos
+        //utilizando programacao defensiva para evitar problemas como adicionar ou limpar a lista sem usar os metodos corretos
         //para isso vamos retornar uma instancia nova de lista de negociacoes
         //vamos usar o metodo concat para criar um novo array com o conteudo de this._negociacoes
         return [].concat(this._negociacoes);
