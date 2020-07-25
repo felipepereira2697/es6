e81f3bf79ae69c23aba18334a8a179db087ab100
Felipe Pereira - PERSONAL <felipepereira2697@gmail.com>
2020-07-25 12:40:24 +0100

    Criando a triade de eventos para lidar com o indexedDB

--- bolsa-valores/client/app/services/ConnectionFactory.js
+++ bolsa-valores/client/app/services/ConnectionFactory.js
@@ -1,3 +1,24 @@
+var stores = ['negociacoes'];
+var version = 3;
+var dbName = 'bolsavalores';
 class ConnectionFactory {
-    
+ 
+    //Evitar que criem instancias dessa classe
+    constructor() {
+        throw new Error('Não é possível criar instancias de ConnectionFactory');
+    }
+    static getConnection() {
+        return new Promise((resolve, reject) => {
+            let openRequest = window.indexedDB.open(dbName, version);
+            openRequest.onupgradeneeded = e => {
+
+            };
+            openRequest.onsuccess = e => {
+
+            }
+            openRequest.onerror = e => {
+
+            }
+        })
+    }
 }
