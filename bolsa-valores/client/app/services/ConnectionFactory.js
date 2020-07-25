15d656fa1e24ddffd99856836f4ca18cb5ba935a
Felipe Pereira - PERSONAL <felipepereira2697@gmail.com>
2020-07-25 13:20:38 +0100

    Finalizando a classe ConnectionFactory, evitando que o desenvolvedor fecha a conexao diretamente pelo connection.close

--- bolsa-valores/client/app/services/ConnectionFactory.js
+++ bolsa-valores/client/app/services/ConnectionFactory.js
@@ -1,10 +1,13 @@
 //Criando uma IIFE, vai ser carregada e ao mesmo tempo será invocada
 const ConnectionFactory = (function()  {
 
-    var stores = ['negociacoes'];
-    var version = 3;
-    var dbName = 'bolsavalores';
-    var connection = null;
+    const stores = ['negociacoes'];
+    const version = 3;
+    const dbName = 'bolsavalores';
+    let connection = null;
+
+    //vai guardar o metodo original
+    let close = null;
 
     //podemos retornar ConnectionFactory para ter acesso 
     return class ConnectionFactory {
@@ -23,6 +26,14 @@ const ConnectionFactory = (function()  {
                     //aqui deu tudo certo
                     if(!connection) {
                         connection = e.target.result;
+
+                        //a variavel close vai receber o metodo connection.close
+                        //para evitar problema do this, vamos usar o bind para copiar a referencia correta
+                        // o this desse cara sempre será a connection
+                        close = connection.close.bind(connection);
+                        connection.close = function() {
+                            throw new Error('Você não pode fechar a conexão diretamente');
+                        }
                     }
                     resolve(connection);
                 }
@@ -32,7 +43,13 @@ const ConnectionFactory = (function()  {
                 }
             })
         }
-    
+        static closeConnection() {
+            if(connection) {
+                //chamando a nossa criacao de close 
+                close();
+                connection = null;
+            }
+        }
         static _createStores(connection) {
             stores.forEach((store) => {
                 //se ja existe uma object store, deleta para criar uma nova
